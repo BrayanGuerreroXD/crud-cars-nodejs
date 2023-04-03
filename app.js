2343f2004fc9c7b6e0787acf3d79244e25cca796
BrayanGuerreroXD <bg7047653@gmail.com>
2023-04-03 10:49:21 -0500

    Add db variables

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import carsRoutes from "../src/routes/cars.routes.js";
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/", carsRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Not found",
+  });
+});
+
+export default app
